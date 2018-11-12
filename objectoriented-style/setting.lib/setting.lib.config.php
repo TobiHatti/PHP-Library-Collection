2017eabc9646e6fc32efe6555929513bd78f9048
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-11-12 20:40:43 +0100

    Updated setting.lib
    Added methods:
    ~ Construct/Deconstruct
    -  [public][static] init()
    
    ~ Public
    -  [public][static] Get()
    -  [public][static] Set()
    -  [public][static] Increment()
    -  [public][static] Decrement()
    
    Added config-file

---
+++ objectoriented-style/setting.lib/setting.lib.config.php
@@ -0,0 +1,18 @@
+<?php
+
+//=================================
+// Connect to MySQL Database
+//=================================
+// Database-Hostname
+$settingConfigDatabaseHost = "localhost";
+
+// Database-Username
+$settingConfigDatabaseUser = "root";
+
+// Database-Password
+$settingConfigDatabasePass = "";
+
+// Database-Name
+$settingConfigDatabaseName = "sample-db";
+
+?>
