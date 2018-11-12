bb834ad71d6ab5812405c197f1589557b3ed348a
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-11-12 20:32:44 +0100

    Updated mysql.lib
    Added methods:
    ~ Construct/Deconstruct
    -  [public][static] init()
    -  [public][static] Close()
    
    ~ Private
    -  [private][static] GetParamTypeList()
    
    ~ Public
    -  [public][static] NonQuery()
    -  [public][static] Scalar()
    -  [public][static] Count()
    -  [public][static] Row()
    -  [public][static] Exist()
    -  [public][static] Fetch()
    -  [public][static] FetchCount()
    -  [public][static] FetchRow()
    -  [public][static] Save()
    -  [public][static] PeriodicSave()
    
    Added config-file

---
+++ objectoriented-style/mysql.lib/mysql.lib.config.php
@@ -0,0 +1,25 @@
+<?php
+
+//=================================
+// Connect to MySQL Database
+//=================================
+// Database-Hostname
+$sqlConfigDatabaseHost = "localhost";
+
+// Database-Username
+$sqlConfigDatabaseUser = "root";
+
+// Database-Password
+$sqlConfigDatabasePass = "";
+
+// Database-Name
+$sqlConfigDatabaseName = "sample-db";
+
+//=================================
+// BackUp-Path
+//=================================
+
+// Path to directory where backups should be stored
+$sqlConfigBackupPath = "backup/";
+
+?>
