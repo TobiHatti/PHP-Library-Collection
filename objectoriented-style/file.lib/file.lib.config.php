30bf19e5c8bafe68150c58e1dfec8d202f5259ea
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-11-26 20:44:58 +0100

    Updated file.lib [OBJ-Style] [#001]
    Added methods:
    ~ Construct/Deconstruct
    -  [public][static] init()
    -  [public] __construct()
    -  [public] Close()
    
    ~ Private
    -  [private][static] MySQLNonQuery()
    -  [private] SaveReplace()
    
    ~ Public
    -  [public] SetFile()
    -  [public] SetPath()
    -  [public] SetName()
    -  [public] SetSQLEntry()
    -  [public] SetFileTypes()
    -  [public] SetMaxFileSize()
    -  [public] Upload()
    
    Added config-file

---
+++ objectoriented-style/file.lib/file.lib.config.php
@@ -0,0 +1,22 @@
+<?php
+
+//=================================
+// Connect to MySQL Database
+//=================================
+// Database-Hostname
+$fileConfigDatabaseHost = "localhost";
+
+// Database-Username
+$fileConfigDatabaseUser = "root";
+
+// Database-Password
+$fileConfigDatabasePass = "";
+
+// Database-Name
+$fileConfigDatabaseName = "sample-db";
+
+
+// Default upload path
+$fileConfigDefaultUploadDirectory = "upload/";
+
+?>
